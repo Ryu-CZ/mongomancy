8795674411dba242b3bbcd076297ea106901d21f
Ryu-CZ <thandeus@gmail.com>
2023-05-12 14:47:52 +0200

    skip version becuas failed pypi push

--- src/mongomancy/__init__.py
+++ src/mongomancy/__init__.py
@@ -1,7 +1,7 @@
 """
 Abstraction layer of PyMongo with auto-reconnect and collection initialization support.
 """
-__version__ = "0.1.8"
+__version__ = "0.1.10"
 
 # pymongo shortcuts
 import pymongo
