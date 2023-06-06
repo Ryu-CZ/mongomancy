153e47448a5f8a68905f30008517ee9d3f503ac0
Ryu-CZ <thandeus@gmail.com>
2023-06-06 09:23:16 +0200

    modify: enable black for 3.11

--- src/mongomancy/__init__.py
+++ src/mongomancy/__init__.py
@@ -6,20 +6,10 @@ __version__ = "0.1.11"
 # pymongo shortcuts
 import pymongo
 
-from . import (
-    types,
-    mongo_errors,
-    engine,
-    schema,
-)
+from . import types, mongo_errors, engine, schema
 from .engine import Engine
 from .schema import Collection, Database
-from .types import (
-    Executor,
-    Index,
-    CollectionDefinition,
-    Document,
-)
+from .types import Executor, Index, CollectionDefinition, Document
 
 ASCENDING = pymongo.ASCENDING
 DESCENDING = pymongo.DESCENDING
